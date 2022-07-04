f8dac3f2888004e9c0807188413926a97ab357f5
Dean Karn <dean.karn@gmail.com>
2022-07-04 16:03:02 -0700

    update docs

--- relay/src/lib.rs
+++ relay/src/lib.rs
@@ -4,7 +4,11 @@
 //!
 //! ### Features
 //! Optional features:
+//! - [`frontend-http`][]: Enables the HTTP frontend (default).
+//! - [`backend-postgres`][]: Enables the Postgres backend (default).
 //! - [`metrics-prometheus`][]: Enables emitting of Prometheus metrics via a scraping endpoint.
 //!
+//! [`frontend-http`]: `relay_frontend_http`
+//! [`backend-postgres`]: `relay_backend_postgres`
 //! [`metrics-prometheus`]: https://crates.io/crates/metrics-exporter-prometheus
 //!
