22c9ab0aceba2d3b733a5105feba8980b20ff763
Dean Karn <dean.karn@gmail.com>
2023-04-23 15:35:43 -0700

    remove ansi from features altogether

--- relay/src/bin/relay.rs
+++ relay/src/bin/relay.rs
@@ -61,7 +61,6 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
         .with_writer(non_blocking)
-        .with_ansi(false)
         .init();
 
     let opts: Opts = Opts::parse();
